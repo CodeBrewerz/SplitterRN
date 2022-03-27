5c07852b9d1c4cd03909153126580d1134d5fae2
Ananya Sharma <ananyas237@gmail.com>
2022-03-27 04:02:47 -0400

    added dark theme toggle

--- App.js
+++ App.js
@@ -1,50 +1,91 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
-
 import React from 'react';
-import { Provider as PaperProvider } from 'react-native-paper';
-import { NavigationContainer } from '@react-navigation/native';
-import { createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs';
 import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
+  DarkTheme as PaperDarkTheme,
+  DefaultTheme as PaperDefaultTheme,
+  Provider as PaperProvider,
+  useTheme, Appbar, Switch,
   Text,
-  useColorScheme,
+  List,
+} from 'react-native-paper';
+import merge from 'deepmerge';
+import {
+  NavigationContainer,
+  DarkTheme as NavigationDarkTheme,
+  DefaultTheme as NavigationDefaultTheme,
+} from '@react-navigation/native';
+import { createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs';
+import { useRecoilState, useRecoilValue } from 'recoil';
+import { PreferencesAtom } from './context/ThemePreferenceContext';
+import {
   View,
 } from 'react-native';
 
+const ThemeToggle = () => {
+  const theme = useTheme();
+  const [isThemeDark, toggleTheme] = useRecoilState(PreferencesAtom);
+
+  return (
+    <Switch
+      color={'red'}
+      value={isThemeDark}
+      onValueChange={() => toggleTheme(prev => !prev)}
+    />
+  )
+
+}
+
+const Header = ({ title }) => {
+
+  return (
+    <Appbar.Header>
+      <Appbar.Content title={title} />
+    </Appbar.Header>
+  );
+};
+
 function HomeScreen() {
   return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+    <View >
+      <Header title="Home" />
       <Text>Home!</Text>
     </View>
   );
 }
 
+const ThemeToggleListItem = () => (
+  <List.Item
+    title="Enable Dark Theme"
+    left={props => <List.Icon {...props} icon="weather-night" />}
+    right={_ => <ThemeToggle />}
+  />
+)
+
+
 function SettingsScreen() {
   return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>Settings!</Text>
+    <View>
+      <Header title="Settings" />
+      <ThemeToggleListItem />
     </View>
   );
 }
 
 const Tab = createMaterialBottomTabNavigator();
 
+const CombinedDefaultTheme = merge(PaperDefaultTheme, NavigationDefaultTheme);
+const CombinedDarkTheme = merge(PaperDarkTheme, NavigationDarkTheme);
+
 const App = () => {
+  const isThemeDark = useRecoilValue(PreferencesAtom);
+
+  const theme = isThemeDark ? CombinedDarkTheme : CombinedDefaultTheme;
+
   return (
-    <NavigationContainer>
-      <PaperProvider>
+    <NavigationContainer theme={theme}>
+      <PaperProvider theme={theme}>
         <Tab.Navigator>
           <Tab.Screen name="Home" component={HomeScreen} />
-          <Tab.Screen name="Transactions" component={SettingsScreen} />
+          <Tab.Screen name="Settings" component={SettingsScreen} />
         </Tab.Navigator>
       </PaperProvider>
     </NavigationContainer>
--- index.js
+++ index.js
@@ -2,9 +2,15 @@
  * @format
  */
 
-import {AppRegistry} from 'react-native';
-
+import { AppRegistry } from 'react-native';
+import React from 'react';
 import App from './App';
-import {name as appName} from './app.json';
+import { RecoilRoot } from 'recoil';
+import { name as appName } from './app.json';
 
-AppRegistry.registerComponent(appName, () => App);
+const Index = () => (
+    <RecoilRoot>
+        <App />
+    </RecoilRoot>
+);
+AppRegistry.registerComponent(appName, () => Index);
