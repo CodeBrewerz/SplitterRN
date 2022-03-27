11e67c3fb5f38aa010bd302c71501ff425cf1423
Ananya Sharma <ananyas237@gmail.com>
2022-03-27 15:13:22 -0400

    add auth list items to settings screen

--- components/Login.js
+++ components/Login.js
@@ -1,24 +1,21 @@
 import React, { useState, useEffect } from 'react';
 import {
     Alert,
-    Button,
-    StyleSheet,
-    Text,
-    View
 } from 'react-native';
 import Auth0 from 'react-native-auth0';
-import { useSetRecoilState, useRecoilState } from 'recoil';
-import { authAccessTokenAtom } from '../context/ThemePreferenceContext';
+import { Avatar, List } from 'react-native-paper';
+import { useRecoilState, useRecoilValue } from 'recoil';
+import { userAuthAtom } from '../context/ThemePreferenceContext';
 
 const AUTH0_CREDENTIALS = {
     clientId: "PwBGZzX1QuaUlNCbyUVff8uEsIgf62im",
     domain: "dev-ezyhf71g.us.auth0.com",
     audience: "https://split-bills-api-gateway-111-0whh5y9mzd71g.apigateway.split-bills-4f198.cloud.goog"
 };
-const auth0 = new Auth0(AUTH0_CREDENTIALS);
 
-export const Login = () => {
-    const [accessToken, setAccessToken] = useRecoilState(authAccessTokenAtom);
+export const LoginListItem = () => {
+    const auth0 = new Auth0(AUTH0_CREDENTIALS);
+    const [accessToken, setAccessToken] = useRecoilState(userAuthAtom);
     useEffect(() => {
         console.log('accessToken: ', accessToken);
     }, [accessToken]);
@@ -29,9 +26,33 @@ export const Login = () => {
                 scope: 'openid profile email',
                 audience: AUTH0_CREDENTIALS.audience
             });
-        setAccessToken(credentials.accessToken);
-    }
+        const userInfo = await auth0.auth.userInfo({ token: credentials.accessToken });
 
+        setAccessToken({ accessToken: credentials.accessToken, info: userInfo });
+    }
+    
+    return (
+        <List.Item
+            title="Sign In"
+            onPress={onLogin}
+        />
+    );
+}
+
+export const LoggedInListItem = () => {
+    const { info } = useRecoilValue(userAuthAtom);
+    return (
+        <List.Item
+            title={info.name || 'Default Fallback'}
+            description={info.email}
+            left={_ => <Avatar.Image size={44} source={{ uri: info.picture || 'https://cdn.pixabay.com/photo/2019/11/03/20/11/portrait-4599553__340.jpg' }} />}
+            right={_ => <List.Icon icon="chevron-right" />}
+        />
+    );
+}
+
+export const LogoutListItem = () => {
+    const auth0 = new Auth0(AUTH0_CREDENTIALS);
     const onLogout = () => {
         auth0.webAuth
             .clearSession({})
@@ -44,26 +65,10 @@ export const Login = () => {
             });
     };
 
-    let loggedIn = accessToken !== null;
     return (
-        <View style={styles.container}>
-            <Text style={styles.header}> Auth0Sample - Login </Text>
-            <Text>You are{loggedIn ? ' ' : ' not '}logged in. </Text>
-            <Button onPress={loggedIn ? onLogout : onLogin}
-                title={loggedIn ? 'Log Out' : 'Log In'} />
-        </View >
+        <List.Item
+            title="Logout"
+            onPress={onLogout}
+        />
     );
 }
-
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-    },
-    header: {
-        fontSize: 20,
-        textAlign: 'center',
-        margin: 10
-    }
-});
