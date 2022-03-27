819c5817eae5cc6e285a01b2a6e248a644461812
Ananya Sharma <ananyas237@gmail.com>
2022-03-27 07:37:59 -0400

    added reconciliations FlatList

--- components/GroupActivity.js
+++ components/GroupActivity.js
@@ -2,18 +2,86 @@ import {
     Appbar,
     Title,
     useTheme,
+    List,
+    Avatar
 } from 'react-native-paper';
-import { StyleSheet, View, Text } from "react-native";
+import { StyleSheet, View, Text, FlatList } from "react-native";
 import {
     Tabs,
     TabScreen,
     useTabIndex,
     useTabNavigation,
 } from 'react-native-paper-tabs';
-import { VictoryBar, VictoryChart, VictoryTheme } from "victory-native";
-
+import { VictoryBar, VictoryChart, VictoryLine, VictoryTheme } from "victory-native";
+import currency from 'currency.js';
 import React from 'react';
+import { SafeAreaView } from 'react-native-safe-area-context';
 
+const transactions = [
+    {
+        id: 1,
+        transactionName: 'Shoppers Drug Mart',
+        description: 'Simon paid for a purchase',
+        amount: 55.32
+    },
+    {
+        id: 2,
+        transactionName: 'Loblaws',
+        description: 'Amanda paid for a purchase',
+        amount: 20.80
+    },
+    {
+        id: 3,
+        transactionName: 'Rogers Internet',
+        description: 'Patrice paid for a purchase',
+        amount: 100
+    },
+    {
+        id: 4,
+        transactionName: '2 - Shoppers Drug Mart',
+        description: 'Simon paid for a purchase',
+        amount: 55.32
+    },
+    {
+        id: 5,
+        transactionName: '2 - Loblaws',
+        description: 'Amanda paid for a purchase',
+        amount: 20.80
+    },
+    {
+        id: 6,
+        transactionName: '2 - Rogers Internet',
+        description: 'Patrice paid for a purchase',
+        amount: 100
+    },
+];
+
+const ReconciliationItem = ({ avatarUrl, transactionName, description, amount }) => (
+    <List.Item
+        title={transactionName || 'Default Fallback'}
+        description={description}
+        left={_ => <Avatar.Image size={44} source={{ uri: avatarUrl || 'https://cdn.pixabay.com/photo/2019/11/03/20/11/portrait-4599553__340.jpg' }} />}
+        right={_ => <List.Subheader>{currency(amount).format()}</List.Subheader>}
+    />
+);
+
+const ReconciliationItems = ({ transactions }) =>
+(<List.Section style={{ padding: 10 }}>
+    <FlatList
+        data={transactions}
+        renderItem={
+            ({ item }) => (
+                <ReconciliationItem
+                    transactionName={item.transactionName}
+                    description={item.description}
+                    amount={item.amount}
+                />
+            )
+        }
+        keyExtractor={item => item.id}
+    />
+</List.Section>
+);
 
 const Chart = () => {
     const styles = StyleSheet.create({
@@ -33,7 +101,7 @@ const Chart = () => {
     return (
         <View style={styles.container}>
             <VictoryChart width={350} theme={VictoryTheme.material}>
-                <VictoryBar data={data} x="quarter" y="earnings" />
+                <VictoryLine data={data} x="quarter" y="earnings" />
             </VictoryChart>
         </View>
     )
@@ -45,31 +113,28 @@ export const GroupActivity = () => {
             <Appbar.Header>
                 <Appbar.Content title="Group Activity" />
             </Appbar.Header>
-            <Chart  />
+            <Chart />
             <Tabs
                 uppercase={false}
                 showTextLabel={true}
                 theme={theme}
-                style={{ borderRadius: 30}}
+                style={{ borderRadius: 30 }}
                 onChangeIndex={(newIndex) => {
                     console.log(newIndex)
                 }} // react on index change
             >
                 <TabScreen label="Incoming">
-                    <View><Text>Incoming</Text></View>
+                    <ReconciliationItems transactions={transactions} />
                 </TabScreen>
                 <TabScreen label="Outgoing">
-                    <Text>Outgoing</Text>
+                    <ReconciliationItems transactions={transactions} />
                 </TabScreen>
                 <TabScreen
                     label="Activity"
                 >
-                    <View>
-                        <Title>Activity</Title>
-
-                    </View>
+                    <ReconciliationItems transactions={transactions} />
                 </TabScreen>
             </Tabs>
         </View>
     )
-}
+};
